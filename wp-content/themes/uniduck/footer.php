d0920c42776b1c4ef4bb9433212635c9bc1d7d24
LidijaCvetkovic <cvetkovic.lidia@gmail.com>
2020-07-12 13:17:48 +0200

    adding code to footer.php file

---
+++ wp-content/themes/uniduck/footer.php
@@ -0,0 +1,20 @@
+</main>
+
+<footer>
+    <div class="container flex">
+        <div class="left flex col-6 pl-0">
+            <img class="logo" src="<?php bloginfo('template_url'); ?>/Assets/Images/logo.png" alt="Uniduck | The Unicorn & a Duck Logo" />
+            <span class="copyright">&copy; <?php echo date('Y'); ?> Uniduck. All rights reserved.</span>
+        </div>
+        <div class="right flex col-6 pr-0">
+            <span>Like <a href="#">Uniduck</a> on <img src="<?php bloginfo('template_url'); ?>/Assets/Icons/ic-facebook.svg" alt="Uniduck | Facebook" /></span>
+            <span>Follow <a href="#">@uniduck</a> on <img src="<?php bloginfo('template_url'); ?>/Assets/Icons/ic-twitter.svg" alt="Uniduck | Twitter" /></span>
+            <span>Follow <a href="#">@uniduck</a> on <img src="<?php bloginfo('template_url'); ?>/Assets/Icons/ic-instagram.svg" alt="Uniduck | Instagram" /></span>
+        </div>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
