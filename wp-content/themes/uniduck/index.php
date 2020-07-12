2bfd344fe63fef4b5bf3476a9aca3302c8d07134
LidijaCvetkovic <cvetkovic.lidia@gmail.com>
2020-07-12 13:31:08 +0200

    adding html code for latest blog post section

--- wp-content/themes/uniduck/index.php
+++ wp-content/themes/uniduck/index.php
@@ -6,6 +6,33 @@
         <input id="search" class="form-control" type="search" placeholder="Search blog" aria-label="Search">
     </section>
 
+    <!-- Last Blog Post Section -->
+    <section class="last-blog-post flex container section-padding">
+        <div class="left col-6 pl-0">
+            <img class="adapt-img" src="Assets/Images/img-unicorn.png" alt="Uniduck | The Unicorn" />
+        </div>
+        <div class="right col-6 pr-0">
+            <span class="date">November 4, 2016</span>
+            <h2 class="last-post-title">The Legend of the Unicorn - Myths and Legends</h2>
+            <div class="tags">
+                <span class="tag">Unicorn</span>
+                <span class="tag">Pinky</span>
+                <span class="tag">Magic</span>
+                <span class="tag">Love</span>
+            </div>
+            <p class="blog-post-excerpt">
+                Once upon a time in kingdom far-far-away in the lands of the never-ending 
+                spring, a king sat in his golden throne and ruled his kingdom in perfect harmony.
+                A person can feel nothing but exuberance at the sight of...
+                <a href="#" class="read-more-btn">Read more</a>
+            </p>
+            <div class="post-engagement flex">
+                <p class="likes"><img src="Assets/Icons/ic-heart.svg" /><span>17 faves</span></p>
+                <p class="comments"><img src="Assets/Icons/ic-comment.svg" /><span>22 comments</span></p>
+            </div>
+        </div>
+    </section>
+
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
 
     <?php endwhile; else : ?>
