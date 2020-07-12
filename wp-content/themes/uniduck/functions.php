44d33b0f39c07fa496486730a53e85f838cc5265
LidijaCvetkovic <cvetkovic.lidia@gmail.com>
2020-07-12 16:21:13 +0200

    including shortcodes.php to functions.php file

--- wp-content/themes/uniduck/functions.php
+++ wp-content/themes/uniduck/functions.php
@@ -3,6 +3,9 @@
 add_theme_support( 'post-thumbnails' );
 add_theme_support('html5',array('search-form'));
 
+// Including shortcodes.php file
+include('shortcodes.php');
+
 // Blog Post Page top widget with title & search bar
 function wpt_create_widget( $name, $id, $description ) {
 
@@ -16,32 +19,6 @@ function wpt_create_widget( $name, $id, $description ) {
 
 }
 wpt_create_widget( 'Blog Page Sidebar', 'blog', 'Displays on the top of the Blog Post Page' );
-
-// Show Latest Post Shortcode
-function latest_post() {
-    $args = array(
-        'posts_per_page' => 1
-    );
-    $query = new WP_Query($args);
-
-    if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>  
-        <div class="left col-6 pl-0">
-            <?php the_post_thumbnail(); ?>
-		</div>
-		<div class="right col-6 pr-0">
-			<span class="date"><?php the_time('F j, Y'); ?></span>
-			<a href="<?php the_permalink(); ?>"><h2 class="last-post-title"><?php the_title(); ?></h2></a>
-			<?php the_tags( '<ul class="tags"><li class="tag">', '</li><li class="tag">', '</li></ul>' ); ?>
-			<p><?php echo wpuni_excerpt(40); ?></p>
-			<div class="post-engagement flex">
-				<p class="likes"><img src="<?php bloginfo('template_url'); ?>/Assets/Icons/ic-heart.svg" /><span>17 faves</span></p>
-				<p class="comments"><img src="<?php bloginfo('template_url'); ?>/Assets/Icons/ic-comment.svg" /><span>22 comments</span></p>
-			</div>
-		</div>
-            
-    <?php endwhile; endif; 
-}
-add_shortcode('lastest-post', 'latest_post');
 
 // Change excerpt length
 function wpuni_excerpt($limit) {
---
+++ wp-content/themes/uniduck/shortcodes.php
@@ -0,0 +1,46 @@
+<?php
+
+// Quote Shortcode [quote][/quote] 
+function shortcode_quote($params = array(), $content) {
+
+    extract(shortcode_atts(array(
+      'type' => ''
+    ), $params));
+  
+    return
+      '<blockquote class="quote"' .
+      ($type ? " class=\"$type\"" : '') .
+      '>' .
+      do_shortcode($content) .
+      '</blockquote>';
+}
+
+add_shortcode('quote', 'shortcode_quote');
+
+// Show Latest Post Shortcode
+function latest_post() {
+    $args = array(
+        'posts_per_page' => 1
+    );
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>  
+        <div class="left col-6 pl-0">
+            <?php the_post_thumbnail(); ?>
+		</div>
+		<div class="right col-6 pr-0">
+			<span class="date"><?php the_time('F j, Y'); ?></span>
+			<a href="<?php the_permalink(); ?>"><h2 class="last-post-title"><?php the_title(); ?></h2></a>
+			<?php the_tags( '<ul class="tags"><li class="tag">', '</li><li class="tag">', '</li></ul>' ); ?>
+			<p><?php echo wpuni_excerpt(40); ?></p>
+			<div class="post-engagement flex">
+				<p class="likes"><img src="<?php bloginfo('template_url'); ?>/Assets/Icons/ic-heart.svg" /><span>17 faves</span></p>
+				<p class="comments"><img src="<?php bloginfo('template_url'); ?>/Assets/Icons/ic-comment.svg" /><span>22 comments</span></p>
+			</div>
+		</div>
+            
+    <?php endwhile; endif; 
+}
+add_shortcode('lastest-post', 'latest_post');
+  
+?>
