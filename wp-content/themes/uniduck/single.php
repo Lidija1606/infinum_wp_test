39937a9af130ecbf024aa69dafbc67c6ccd20cc4
LidijaCvetkovic <cvetkovic.lidia@gmail.com>
2020-07-12 13:22:00 +0200

    changing code for single.php file

--- wp-content/themes/uniduck/single.php
+++ wp-content/themes/uniduck/single.php
@@ -1,22 +1,9 @@
-<?php
+<?php get_header(); ?>
 
-function wpuni_theme_styles() {
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
 
-    // Bootstrap CSS
-    wp_enqueue_style( 'bootstrap_css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' );
-
-    // Main CSS
-    wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
-
-}
-add_action( 'wp_enqueue_scripts', 'wpuni_theme_styles' );
-
-function wpuni_theme_js() {
-
-    // Bootstrap JS
-    wp_enqueue_script( 'bootstrap_js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array('jquery'), '', true );
-
-}
-add_action( 'wp_enqueue_scripts', 'wpuni_theme_js' );
-
-?>
+    <?php endwhile; else : ?>
+        <p><?php esc_html_e( 'Sorry, no pages found.' ); ?></p>
+    <?php endif; ?>
+			
+<?php get_footer(); ?>
