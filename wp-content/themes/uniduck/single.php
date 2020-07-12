7fd0d6c22015120697d9594c4e74cb55367f3fed
LidijaCvetkovic <cvetkovic.lidia@gmail.com>
2020-07-12 18:34:52 +0200

    adding related post section on single post page

--- wp-content/themes/uniduck/single.php
+++ wp-content/themes/uniduck/single.php
@@ -22,6 +22,40 @@
             <?php the_content(); ?>
         </section>
 
+        <!-- Related Posts Section -->
+        <section class="container related-post section-padding">
+            <h4>More Magic</h4>
+            <?php
+                $tags = wp_get_post_tags($post->ID);
+
+                if ($tags) {
+                    $first_tag = $tags[0]->term_id;
+                    $args=array(
+                    'tag__in' => array($first_tag),
+                    'post__not_in' => array($post->ID),
+                    'posts_per_page'=>1,
+                    'caller_get_posts'=>1
+                    );
+                    $my_query = new WP_Query($args);
+
+                    if( $my_query->have_posts() ) {
+                    while ($my_query->have_posts()) : $my_query->the_post(); ?>
+                        <div class="post-item">
+                            <div class="image">
+                                <?php the_post_thumbnail(); ?>
+                            </div>
+                            <div class="post-text">
+                                <a href="<?php the_permalink(); ?>"><h3 class="last-post-title"><?php the_title(); ?></h3></a>                                
+                                <p><?php echo wpuni_excerpt(11); ?></p>
+                            </div>
+                        </div>
+                    <?php endwhile;
+                    }
+                    wp_reset_query();
+                }
+            ?>
+        </section>
+
     <?php endwhile; else : ?>
         <p><?php esc_html_e( 'Sorry, no pages found.' ); ?></p>
     <?php endif; ?>
