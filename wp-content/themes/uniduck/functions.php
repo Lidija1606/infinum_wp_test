e28b4f68c00bea6d98ba5a0cae7290c35da9019f
LidijaCvetkovic <cvetkovic.lidia@gmail.com>
2020-07-12 13:37:35 +0200

    adding shortcode function for displaying latest post

--- wp-content/themes/uniduck/functions.php
+++ wp-content/themes/uniduck/functions.php
@@ -1,6 +1,32 @@
 <?php
 
-// Change excerpt lenght
+// Show Latest Post Shortcode
+function latest_post() {
+    $args = array(
+        'posts_per_page' => 1
+    );
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>  
+        <div class="left col-6 pl-0">
+            <?php the_post_thumbnail(); ?>
+		</div>
+		<div class="right col-6 pr-0">
+			<span class="date"><?php the_date(); ?></span>
+			<a href="<?php the_permalink(); ?>"><h2 class="last-post-title"><?php the_title(); ?></h2></a>
+			<?php the_tags( '<ul class="tags"><li class="tag">', '</li><li class="tag">', '</li></ul>' ); ?>
+			<p><?php echo wpuni_excerpt(40); ?></p>
+			<div class="post-engagement flex">
+				<p class="likes"><img src="<?php bloginfo('template_url'); ?>/Assets/Icons/ic-heart.svg" /><span>17 faves</span></p>
+				<p class="comments"><img src="<?php bloginfo('template_url'); ?>/Assets/Icons/ic-comment.svg" /><span>22 comments</span></p>
+			</div>
+		</div>
+            
+    <?php endwhile; endif; 
+}
+add_shortcode('lastest-post', 'latest_post');
+
+// Change excerpt length
 function wpuni_excerpt($limit) {
     $more = sprintf( '<a class="read-more" href="%1$s">%2$s</a>',
         get_permalink( get_the_ID() ),
@@ -29,7 +55,7 @@ function content($limit) {
     } else {
         $content = implode(" ",$content);
     }
-    
+
     $content = preg_replace('/[.+]/','', $content);
     $content = apply_filters('the_content', $content);
     $content = str_replace(']]>', ']]>', $content);
