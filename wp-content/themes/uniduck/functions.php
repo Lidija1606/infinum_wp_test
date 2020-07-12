9ad59e09d28a02dbba9c41344b3048970ec9b9de
LidijaCvetkovic <cvetkovic.lidia@gmail.com>
2020-07-12 13:35:56 +0200

    adding functions for post excerpt with custom length in functions.php file

--- wp-content/themes/uniduck/functions.php
+++ wp-content/themes/uniduck/functions.php
@@ -1,5 +1,41 @@
 <?php
 
+// Change excerpt lenght
+function wpuni_excerpt($limit) {
+    $more = sprintf( '<a class="read-more" href="%1$s">%2$s</a>',
+        get_permalink( get_the_ID() ),
+        __( ' Read More', 'textdomain' )
+    );
+
+    $excerpt = explode(' ', get_the_excerpt(), $limit);
+
+    if (count($excerpt)>=$limit) {
+        array_pop($excerpt);
+        $excerpt = implode(" ",$excerpt).'...'.$more;
+    } else {
+        $excerpt = implode(" ",$excerpt);
+    }
+
+    $excerpt = preg_replace('`[[^]]*]`','',$excerpt);
+    return $excerpt;
+}
+    
+function content($limit) {
+    $content = explode(' ', get_the_content(), $limit);
+
+    if (count($content)>=$limit) {
+        array_pop($content);
+        $content = implode(" ",$content).'...'.$more;
+    } else {
+        $content = implode(" ",$content);
+    }
+    
+    $content = preg_replace('/[.+]/','', $content);
+    $content = apply_filters('the_content', $content);
+    $content = str_replace(']]>', ']]>', $content);
+    return $content;
+}
+
 function wpuni_theme_styles() {
 
     // Bootstrap CSS
