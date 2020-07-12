3b54be0b77b7220fdfacd170c4f093595b551b7b
LidijaCvetkovic <cvetkovic.lidia@gmail.com>
2020-07-12 19:57:12 +0200

    editing class for blog post item

--- wp-content/themes/uniduck/index.php
+++ wp-content/themes/uniduck/index.php
@@ -21,7 +21,7 @@
 		?>
 
             <!-- Blog Post Item -->
-			<div class="col-4 post-item">
+			<div class="col-12 col-sm-6 col-lg-4 post-item">
 				<div class="image">
 					<?php the_post_thumbnail(); ?>
 				</div>
